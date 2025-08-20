987d79e12ccaf17819073d43d8d2bebaaaba094b
pedroServfaz <pedro.servfaz@gmail.com>
2025-08-20 15:32:47 -0300

    commit teste

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, StatusBar } from "react-native";
 import {
   useFonts,
   Inter_400Regular,
@@ -7,6 +6,7 @@ import {
   Inter_700Bold,
   Inter_800ExtraBold,
 } from "@expo-google-fonts/inter";
+
 import { Loading } from "./src/components/Loading";
 
 export default function App() {
@@ -26,7 +26,11 @@ export default function App() {
       <Text style={styles.text}>
         Open up App.tsx to start working on your app!
       </Text>
-      <StatusBar style="auto" />
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
